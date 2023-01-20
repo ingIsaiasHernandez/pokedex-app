d60d6e2d22167a1740ead7dab4ea083a508751ca
ingIsaiasHernandez <ing.isaias.hdez@gmail.com>
2023-01-19 19:09:09 -0600

    [add] - the principal configuration of Router library was configurated

---
+++ src/components/InputPokemonName.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InputPokemonName = () => {
+    return (
+        <div>
+            <h1>Input Pokemon name</h1>
+        </div>
+    );
+};
+
+export default InputPokemonName;
---
+++ src/components/PokemonDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PokemonDetail = () => {
+    return (
+        <div>
+            <h1>Pokemon detail</h1>
+            
+        </div>
+    );
+};
+
+export default PokemonDetail;
